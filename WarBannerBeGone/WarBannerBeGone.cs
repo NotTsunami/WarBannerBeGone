bb7863cffe3971aaff8cbb9772bbc94a00bce4b9
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2020-02-06 14:32:30 -0500

    Initial commit

---
+++ WarBannerBeGone/WarBannerBeGone.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using RoR2;
+using ItemCatalog = On.RoR2.ItemCatalog;
+
+namespace Tsunami
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("dev.tsunami.WarBannerBeGone", "WarBannerBeGone", "1.0.0")]
+    public class WarBannerBeGone : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            ItemCatalog.RegisterItem += (orig, index, def) =>
+            {
+                if (index == ItemIndex.WardOnLevel)
+                {
+                    def.tier = ItemTier.NoTier;
+                }
+                orig(index, def);
+            };
+        }
+    }
+}
